9bb7aa153630d0b49c23e2db8bd4faac03a2019c
JeanV21 <jeanp-55@outlook.es>
2024-02-23 14:07:39 -0500

    changes made

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__, template_folder='config/templates')
 def inicio():
     return render_template('index.html')
 
-@app.route('/productos')
-def productos():
-    return render_template('productos.html')
-
 @app.route('/login')
 def admin_index():
     return render_template('login.html')
