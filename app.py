ce91f6e36afb1387a98bfd2f5b48328ff019ed33
JeanV21 <jeanp-55@outlook.es>
2024-03-16 14:09:53 -0500

    UPDATE CODE

--- app.py
+++ app.py
@@ -1,18 +1,79 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='config/templates')
+app = Flask(__name__, template_folder='config/templates', static_folder='config/static')
 
 @app.route('/')
-def inicio():
-    return render_template('index.html')
-
-@app.route('/login')
-def admin_index():
+def login():
     return render_template('login.html')
 
-@app.route('/register')  
-def register():
-    return render_template('register.html')
+# Rutas relacionadas con las compras
+@app.route('/compras')
+def compras():
+    return render_template('rolEmpresa/compras.html')
+
+@app.route('/comprasVendedor')
+def comprasVendedor():
+    return render_template('rolVendedor/compras.html')
+# --------------------------------------------------
+
+@app.route('/productosVendedor')
+def productosVendedor():
+    return render_template('rolVendedor/productos.html')
+
+
+@app.route('/productos')
+def productos():
+    return render_template('rolEmpresa/productos.html')
+
+
+
+@app.route('/clientesVendedor')
+def clientesVendedor():
+    return render_template('rolVendedor/clientes.html')
+
+
+@app.route('/clientes')
+def clientes():
+    return render_template('rolEmpresa/clientes.html')
+
+
+
+
+@app.route('/cotizaciones')
+def cotizaciones():
+    return render_template('rolEmpresa/cotizaciones.html')
+
+
+
+@app.route('/informes')
+def informes():
+    return render_template('rolEmpresa/informes.html')
+
+
+
+@app.route('/vendedores')
+def vendedores():
+    return render_template('rolEmpresa/vendedores.html')
+
+@app.route('/stock')
+def stock():
+    return render_template('rolEmpresa/stock.html')
+
+@app.route('/parametros')
+def parametros():
+    return render_template('rolEmpresa/parametros.html')
+
+@app.route('/salir')
+def salir():
+    return render_template('login.html')
+
+@app.route('/mainE')
+def mainE():
+    return render_template('rolEmpresa/main.html')
+
+@app.route('/mainV')
+def mainV():
+    return render_template('rolVendedor/main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
