a50f7430b71ca9ed01d9769ad02f53528588ba20
JeanV21 <103226294+MrCerdito@users.noreply.github.com>
2024-04-14 12:54:13 -0500

    CAMBIO

--- config/static/js/formularios.js
+++
@@ -1,9 +0,0 @@
-function mostrarFormulario() {
-    var formulario = document.getElementById("crear-form-container");
-    formulario.classList.add("active");
-}
-
-function cerrarFormulario() {
-    var formulario = document.getElementById("crear-form-container");
-    formulario.classList.remove("active");
-}
